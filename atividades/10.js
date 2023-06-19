e4283c3d0d5f40b34a6dfaf356472949c471f3e1
jpbroering <102531849+jpbroering@users.noreply.github.com>
2023-06-19 10:59:23 -0300

    Add files via upload

---
+++ atividades/10.js
@@ -0,0 +1,19 @@
+var melhoresAlunos = ({
+    Augusto:[8, 7.6, 8.9, 6], // media 7.625
+    Mariana:[9, 6.6, 7.9, 8], // media 7.875
+    Carla:[7, 7, 8, 9] // media 7.75
+    })
+function media(list){
+    for(e in list){
+        let c = 0
+        var r = 0
+        list[e].forEach(element => {
+            c =c+element
+            r = c
+        });
+        console.log(r)
+        var cM = r/list[e].length
+        console.log(`o aluno ${e} te a media ${cM}`)
+    }
+}
+media(melhoresAlunos)
